1ebd436ab8c57d973d044963910110f1154dd657
Mumbi Francis Kinyanjui <kinya.francis.mumbi@gmail.com>
2021-04-24 23:51:21 +0300

    added POW consensus algorithm

---
+++ proof-of-work/main.go
@@ -0,0 +1,146 @@
+package main
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/binary"
+	"fmt"
+	"log"
+	"math"
+	"math/big"
+	"strconv"
+	"time"
+)
+
+type Block struct {
+	Timestamp     int64
+	Data          []byte
+	PrevBlockHash []byte
+	Hash          []byte
+	Nonce         int
+}
+
+type Blockchain struct {
+	blocks []*Block
+}
+
+type ProofOfWork struct {
+	block  *Block
+	target *big.Int
+}
+
+const targetBits = 24
+
+//NewGenesisBlock creates the first Block in the chain
+func NewGenesisBlock() *Block {
+	return NewBlock("ET Genesis Block", []byte{})
+}
+
+//NewBlockChain creates a new blockchain
+func NewBlockchain() *Blockchain {
+	return &Blockchain{[]*Block{NewGenesisBlock()}}
+}
+
+func NewProofOfWork(b *Block) *ProofOfWork {
+	target := big.NewInt(1)
+	target.Lsh(target, uint(256-targetBits))
+
+	pow := &ProofOfWork{b, target}
+
+	return pow
+}
+
+func (pow *ProofOfWork) prepareData(nonce int) []byte {
+	data := bytes.Join([][]byte{
+		pow.block.PrevBlockHash,
+		pow.block.Data,
+		IntToHex(pow.block.Timestamp),
+		IntToHex(int64(targetBits)),
+		IntToHex(int64(nonce)),
+	},
+		[]byte{},
+	)
+
+	return data
+}
+
+func IntToHex(num int64) []byte {
+	buff := new(bytes.Buffer)
+	err := binary.Write(buff, binary.BigEndian, num)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return buff.Bytes()
+}
+
+func (pow *ProofOfWork) Run() (int, []byte) {
+	var hashInt big.Int
+	var hash [32]byte
+	nonce := 0
+
+	fmt.Printf("Mining the Block containing \"%s\"\n", pow.block.Data)
+	for nonce < math.MaxInt64 {
+		data := pow.prepareData(nonce)
+		hash = sha256.Sum256(data)
+		fmt.Printf("\r%x", hash)
+		hashInt.SetBytes(hash[:])
+
+		if hashInt.Cmp(pow.target) == -1 {
+			break
+		} else {
+			nonce++
+		}
+	}
+	fmt.Println()
+
+	return nonce, hash[:]
+}
+
+//Validate validates that the POW works
+func (pow *ProofOfWork) Validate() bool {
+	var hashInt big.Int
+
+	data := pow.prepareData(pow.block.Nonce)
+	hash := sha256.Sum256(data)
+	hashInt.SetBytes(hash[:])
+
+	isValid := hashInt.Cmp(pow.target) == -1
+
+	return isValid
+}
+
+//NewBlock creates a new Block
+func NewBlock(data string, prevBlockHash []byte) *Block {
+	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}, 0}
+	pow := NewProofOfWork(block)
+	nonce, hash := pow.Run()
+
+	block.Hash = hash[:]
+	block.Nonce = nonce
+
+	return block
+}
+
+// AddBlock adds a new Block in the Blockchain
+func (bc *Blockchain) AddBlock(data string) {
+	prevBlock := bc.blocks[len(bc.blocks)-1]
+	newBlock := NewBlock(data, prevBlock.Hash)
+	bc.blocks = append(bc.blocks, newBlock)
+}
+
+func main() {
+	bc := NewBlockchain()
+
+	bc.AddBlock("Block 001")
+	bc.AddBlock("heheh 002")
+
+	for _, block := range bc.blocks {
+		fmt.Printf("Prev Hash : %x\n", block.PrevBlockHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+		pow := NewProofOfWork(block)
+		fmt.Printf("POW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
+	}
+
+}
